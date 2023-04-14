45f0505b8ba100962e3087902d1c434c779bb39c
VekkaAi <130788280+VekkaAi@users.noreply.github.com>
2023-04-15 02:58:28 +0900

    Delete sd-lock-ui-extension directory

--- sd-lock-ui-extension/preload.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from scripts.editor import Editor
-
-def preload(parser):
-    pass
--- sd-lock-ui-extension/scripts/editor.py
+++
@@ -1,5 +0,0 @@
-# scripts/editor.py
-
-class Editor:
-    def __init__(self):
-        self.gui = None
